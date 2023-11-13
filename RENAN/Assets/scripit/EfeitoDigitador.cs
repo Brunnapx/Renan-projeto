b82d2714eed5bcd0a17efed2850a858e08def83d
Bruna Mirelly Ferreira Paixão <20211170150070@ifrn.local>
2023-11-13 16:14:07 -0300

    text em andamento

---
+++ RENAN/Assets/scripit/EfeitoDigitador.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using TMPro;
+using UnityEngine;
+
+public class EfeitoDigitador : MonoBehaviour
+{
+    private TextMeshProUGUI compTexto;
+    private string mensagemOriginal;
+    public float TempoPadrao = 0.04f;
+
+
+
+    private void Awake()
+    {
+        TryGetComponent(out compTexto);
+        mensagemOriginal = compTexto.text;
+        compTexto.text = "";
+    }
+
+    public void ExibirMensagem(string msg)
+    {
+        StartCoroutine(LetraPorLetra(msg));
+    }
+
+    IEnumerator LetraPorLetra(string mensagem)
+    {
+        string msg = "";
+        foreach (var Letra in mensagem)
+        {
+            msg += Letra;
+            compTexto.text = msg;
+            //if(Letra)
+            yield return new WaitForSeconds(TempoPadrao);
+        }
+        StopCoroutine(LetraPorLetra(mensagem));
+    }
+}
